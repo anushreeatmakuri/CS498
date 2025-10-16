a0ac4eb9b876c71de518752340395655d1be9ab2
anushreeatmakuri <143158890+anushreeatmakuri@users.noreply.github.com>
2025-10-15 22:56:04 -0500

    adding tmp

--- Homework/hw1/q3.py
+++ Homework/hw1/q3.py
@@ -75,7 +75,7 @@ def ring_allreduce_(tensor: torch.Tensor, world_size = None, rankid = None):
     #we provide the reduce_scatter and all_gather func prototype for you
     # You may adjust the function signature (input structure) of `reduce_scatter` and `all_gather` if needed.
     tmp = torch.zeros_like(chunks[0])
-    reduce_scatter(chunks, world, rank, left, right)
+    reduce_scatter(chunks, tmp, world, rank, left, right)
     all_gather(chunks, tmp, None, world, rank, left, right)
 
     # stitch & unpad  
