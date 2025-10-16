0ff6788a349574125de5c8eada06d629a812eec5
anushreeatmakuri <143158890+anushreeatmakuri@users.noreply.github.com>
2025-10-15 23:00:54 -0500

    switching directions

--- Homework/hw1/q3.py
+++ Homework/hw1/q3.py
@@ -16,8 +16,8 @@ def reduce_scatter(chunks, tmp, world, rank, left, right):
         send_chunk_idx = (rank - i) % world
         recv_chunk_idx = (rank - i - 1) % world
         
-        send_req = dist.isend(chunks[send_chunk_idx], dst=right)
-        recv_req = dist.irecv(tmp, src=left)
+        send_req = dist.isend(chunks[send_chunk_idx], dst=left)   # send to left
+        recv_req = dist.irecv(tmp, src=right)  
         
         send_req.wait()
         recv_req.wait()
@@ -35,8 +35,8 @@ def all_gather(chunks, tmp, current, world, rank, left, right):
         send_chunk_idx = (rank - i) % world
         recv_chunk_idx = (rank - i - 1) % world
         
-        send_req = dist.isend(chunks[send_chunk_idx], dst=right)
-        recv_req = dist.irecv(tmp, src=left)
+        send_req = dist.isend(chunks[send_chunk_idx], dst=left)
+        recv_req = dist.irecv(tmp, src=right)
         
         send_req.wait()
         recv_req.wait()
